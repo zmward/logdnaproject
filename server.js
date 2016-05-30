8cc92671d6c7dce0a8324580a9e3fde294f148a2
Zach <Aftermath1991@gmail.com>
2016-05-30 11:59:02 -0700

    server working and tested in PostMan

---
+++ server.js
@@ -0,0 +1,16 @@
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var router      =   express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
+});
+
+app.use('/',router);
+
+app.listen(3000);
+console.log("Listening to PORT 3000");
